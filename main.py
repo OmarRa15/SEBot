937420221ea31fcb22d9cd6b0e8d61d22fab95cb
OmarRa15 <omarhoms0@gmail.com>
2022-01-28 14:28:33 +0300

    Making the bot give the message id for Admin's messages 3

--- main.py
+++ main.py
@@ -38,14 +38,12 @@ def resources(message):
 
 # For Administrators' Debugging
 def message_id_boolean(message):
-    return True
-    # return message.from_user.username.lower() == 'kasehomar'
+    return message.from_user.username.lower() == 'kasehomar'
 
 
 @bot.message_handler(func=message_id_boolean)
 def message_id(message):
     bot.reply_to(message, str(message.message_id))
-    bot.reply_to(message, str(message.from_user.username.lower()))
 
 
 bot.polling()
